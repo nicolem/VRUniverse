8cd94d1dc592a7512200b033ea81f6daf6d9493e
Emily Smith <easmith5@ku.edu>
2016-02-28 16:28:52 -0600

    Sun Particles Added. One click transfer working.

---
+++ Assets/ChangeScenesfromSun.js
@@ -0,0 +1,30 @@
+﻿var platform : RuntimePlatform = Application.platform;
+
+function Update () {
+    
+    if(platform == RuntimePlatform.Android || platform == RuntimePlatform.IPhonePlayer)
+    {
+        if(Input.touchCount == 1) 
+        {
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Application.LoadLevel("SunParticles");
+            }
+        }
+        else if(Input.touchCount == 2) 
+        {
+            if(Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                //Load Menu Scene
+            }
+        }
+    }
+    else if(platform == RuntimePlatform.OSXEditor || platform == RuntimePlatform.WindowsEditor){
+
+        if(Input.GetMouseButtonDown(0)) {
+            Debug.Log("code is running");
+            Application.LoadLevel("SunParticles");
+            Debug.Log("buttn pressed");
+        }
+    }
+}

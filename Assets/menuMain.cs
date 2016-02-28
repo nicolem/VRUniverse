789abe204bb6c11d371b8b5bca8041e1b1beb844
Christina7 <147.christina.s@gmail.com>
2016-02-28 16:59:21 -0600

    Menu :dancers:
    transitions to earth and sun from menu :D

---
+++ Assets/menuMain.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class menuMain : MonoBehaviour
+{
+
+    // ....I don't really know what I'm doing here
+    public GameObject sun;
+    public GameObject earth;
+
+    // Use this for initialization
+    void Start()
+    {
+        Cardboard.Create();
+
+        sun = GameObject.FindGameObjectWithTag("SUN");
+        earth = GameObject.FindGameObjectWithTag("EARTH");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // yoyoyo do nothing
+    }
+
+    // hopefully it changes the text color on clicks
+    public void sunPress()
+    {
+        Debug.Log("in sun \n");
+        Application.LoadLevel("sun");
+
+    }
+
+    //does the same 
+    public void earthPress()
+    {
+
+
+        Debug.Log("in earth \n");
+        Application.LoadLevel("earth");
+
+    }
+}

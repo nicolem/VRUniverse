807c3997c9d845cda1b31193402a2e8b76ec7a3b
Emily Smith <easmith5@ku.edu>
2016-04-26 16:22:31 -0500

    Saturn and Uranus Molecules
    Okay, so Saturn and Jupiter molecule scenes are actually the layer of
    liquid hydrogen just outside the cores of those planets cause we don't
    know what's actually in the core. The Uranus core has water, methane and
    ammonia, so I just approximated a sphere for those molecules. But it's
    super colorful and cool looking! :-)

---
+++ Assets/ChangeScenesfromSaturn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScenesfromSaturn : MonoBehaviour {
+    // Use this for initialization
+    void Start()
+    {
+        Cardboard.SDK.VRModeEnabled = ApplicationModel.vrbool;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("SaturnMolecules");
+        }
+    }
+}
